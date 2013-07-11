1e3f48808e61b5d4e47ba82f8529b09cd3549c36
Waverunner <waverun@live.com>
2013-07-11 18:27:20 -0400

    Updated some RadialOptions()

--- scripts/radial/chance_cube.py
+++ scripts/radial/chance_cube.py
@@ -5,14 +5,18 @@ from java.util import Vector
 import sys
 
 def createRadial(core, owner, target, radials):
-	radials.clear()
+
+	radials.add(RadialOptions(0, 21, 1, '')) #Use
 	radials.add(RadialOptions(0, RadialOptions.diceRoll, 3, '@dice/dice:dice_roll_single'))
-	radials.add(RadialOptions(0, 7, 1, ''))
+	radials.add(RadialOptions(0, 7, 1, '')) #Examine
+	#radials.add(RadialOptions(0, 24, 1, '')) #0
+	#radials.add(RadialOptions(0, 25, 1, ''))
+	radials.add(RadialOptions(0, 26, 1, ''))
+	#radials.add(RadialOptions(0, 27, 1, ''))
 	return
 
 def handleSelection(core, owner, target, option):
-	if option == RadialOptions.diceRoll:
-		print 'dice rolled'
-		owner.sendSystemMessage('You rolled nothing trololol')
+	if option == RadialOptions.diceRoll or 21:
+		owner.sendSystemMessage('You rolled nothing trololol', 0)
 	return
 	
